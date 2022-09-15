cde79774105657a488dacdf97065eeb84891372f
Oscar <oscar.gi.cast@gmail.com>
2022-09-15 00:27:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+# Dynamically tipado.
+# Int.
+# # Estp es un comentario.
+# x = 100
+# print(x)
+# print(type(x))
+
+# Float.
+# x = 0.1
+# print(x)
+# print(type(x))
+
+# x = 'Hola python'
+# print(x)
+# print(type(x))
+
+# x = True
+# print(x)
+# print(type(x))
+
+# Booleans.
+# print('hola' == 'chau')
+# print('hola' != 'chau')
+# print(1 == 2)
+# print(1 + 3 == 2 + 2)
+# print(1.4 + 1.6 == 3)
+# print(99/2)
+# print(99//2)
+# print(9 / 2)
+# print(9 // 2)
+# print(9 % 2)
+# print(0.1 + 0.2)
+# print(1/3)
+# print(0.1 + 0.2)
+
+# x = None
+# # print(x)
+# # print(type(x))
+
+# print(x is None)
+
+# # Strongly typed.
+# suma = 100 + 200
+# print(suma)
+# # suma = 'abc' + 4
+
+# Lists.
+# primes = [1, 2, 3, 5, 7]
+# print(type(primes))
+# print(primes)
+# print(primes[0])
+
+# greeting = 'Hola mundo'
+# print(greeting[0])
+# print(greeting[-1])
+
+# # Modifying a list, mutable object.
+
+# # # Tuple.
+# frutos = ('manzana', 'fresa', 'aguaymanto')
+# print(frutos)
+# print(type(frutos))
+
+mapa = {
+    'python': 'Lenguaje de programación que usaremos en el curso',
+    'c++': 'usando en unuversidades',
+}
+
+print(mapa['python'])
+
+edades_clase = {'rodmel': 20, 'hermez': 18, 'jose': 30}
+
+print(edades_clase['jose'])
+
+edades_clase
